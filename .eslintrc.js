25e9dc79311d3c74a61482aacde9773fa5e26f27
baitanggao <baitanggao@2dfire.com>
2023-03-15 10:24:04 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: "babel-eslint", // 解析器
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  extends: [], // 扩展
+  plugins: [], // 插件
+  globals: {
+  },
+  rules: {},
+};
